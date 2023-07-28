f4f8c0aef5af4e7ad24171df474fd560452ac557
Mehmet Fevzi Akdeniz <m.fevziakdeniz@gmail.com>
2023-07-28 12:17:37 +0300

    ilk commit

---
+++ product-service/src/main/java/com/eleiatech/stockmanagement/productservice/controller/ProductController.java
@@ -0,0 +1,60 @@
+package com.eleiatech.stockmanagement.productservice.controller;
+
+import com.eleiatech.stockmanagement.productservice.enums.Language;
+import com.eleiatech.stockmanagement.productservice.exception.enums.FriendlyMessageCodes;
+import com.eleiatech.stockmanagement.productservice.exception.utils.FriendlyMessageUtils;
+import com.eleiatech.stockmanagement.productservice.repository.entity.Product;
+import com.eleiatech.stockmanagement.productservice.request.ProductCreateRequest;
+import com.eleiatech.stockmanagement.productservice.request.ProductUpdateRequest;
+import com.eleiatech.stockmanagement.productservice.response.FriendlyMessage;
+import com.eleiatech.stockmanagement.productservice.response.InternalApiResponse;
+import com.eleiatech.stockmanagement.productservice.response.ProductResponse;
+import com.eleiatech.stockmanagement.productservice.service.IProductRepositoryService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/api/1.0/product")
+@RequiredArgsConstructor
+class ProductController {
+    private final IProductRepositoryService productRepositoryService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/{language}/create")
+    public InternalApiResponse<ProductResponse> createProduct(@PathVariable("language") Language language,
+                                                              @RequestBody ProductCreateRequest productCreateRequest) {
+        log.debug("[{}][createProduct] -> request: {}", this.getClass().getSimpleName(), productCreateRequest);
+        Product product = productRepositoryService.createProduct(language, productCreateRequest);
+        ProductResponse productResponse = convertProductResponse(product);
+        log.debug("[{}][createProduct] -> response: {}", this.getClass().getSimpleName(), productResponse);
+        return InternalApiResponse.<ProductResponse>builder()
+                .friendlyMessage(FriendlyMessage.builder()
+                        .title(FriendlyMessageUtils.getFriendlyMessage(language, FriendlyMessageCodes.SUCCESS))
+                        .description(FriendlyMessageUtils.getFriendlyMessage(language, FriendlyMessageCodes.PRODUCT_SUCCESSFULLY_CREATED))
+                        .build())
+                .httpStatus(HttpStatus.CREATED)
+                .hasError(false)
+                .payload(productResponse)
+                .build();
+
+    }
+
+    private ProductResponse convertProductResponse(Product product) {
+        return ProductResponse.builder()
+                .productId(product.getProductId())
+                .productName(product.getProductName())
+                .quantity(product.getQuantity())
+                .price(product.getPrice())
+                .productCreatedDate(product.getProductCreatedDate().getTime())
+                .productUpdatedDate(product.getProductUpdatedDate().getTime())
+                .build();
+    }
+
+}
---
+++ product-service/src/main/java/com/eleiatech/stockmanagement/productservice/service/impl/ProductRepositoryServiceImpl.java
@@ -0,0 +1,61 @@
+package com.eleiatech.stockmanagement.productservice.service.impl;
+
+import com.eleiatech.stockmanagement.productservice.enums.Language;
+import com.eleiatech.stockmanagement.productservice.exception.enums.FriendlyMessageCodes;
+import com.eleiatech.stockmanagement.productservice.exception.exceptions.ProductNotCreatedException;
+import com.eleiatech.stockmanagement.productservice.repository.ProductRepository;
+import com.eleiatech.stockmanagement.productservice.repository.entity.Product;
+import com.eleiatech.stockmanagement.productservice.request.ProductCreateRequest;
+import com.eleiatech.stockmanagement.productservice.request.ProductUpdateRequest;
+import com.eleiatech.stockmanagement.productservice.service.IProductRepositoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductRepositoryServiceImpl implements IProductRepositoryService {
+    private final ProductRepository productRepository;
+
+    @Override
+    public Product createProduct(Language language, ProductCreateRequest productCreateRequest) {
+        log.debug("[{}][createProduct] -> request: {}", this.getClass().getSimpleName(), productCreateRequest);
+        try {
+            Product product = Product.builder()
+                    .productName(productCreateRequest.getProductName())
+                    .quantity(productCreateRequest.getQuantity())
+                    .price(productCreateRequest.getPrice())
+                    .deleted(false)
+                    .build();
+            Product productResponse = productRepository.save(product);
+            log.debug("[{}][createProduct] -> response: {}", this.getClass().getSimpleName(), productResponse);
+            return productResponse;
+        } catch (Exception exception) {
+            throw new ProductNotCreatedException(language, FriendlyMessageCodes.PRODUCT_NOT_CREATED_EXCEPTION, "product request: " + productCreateRequest.toString());
+        }
+    }
+
+    @Override
+    public Product getProduct(Language language, Long productId) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getProducts(Language language) {
+        return null;
+    }
+
+    @Override
+    public Product updateProduct(Language language, Long productId, ProductUpdateRequest productUpdateRequest) {
+        return null;
+    }
+
+    @Override
+    public Product deleteProduct(Language language, Long productId) {
+        return null;
+    }
+}
